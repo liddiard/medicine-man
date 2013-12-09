018b04507a05b3c738a9ea2b2d07e0a4ddce847e
omniaura <omniaura5@gmail.com>
2013-12-09 11:56:52 -0800

    url-to-template routing for frontend development testing

--- location/views.py
+++ location/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class FrontView(TemplateView):
+
+    template_name = "location/front.html"
--- medman/urls.py
+++ medman/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from location import views as location_views
+from content import views as content_views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'medman.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', location_views.FrontView.as_view(), name='front'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
