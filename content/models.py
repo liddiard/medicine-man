19c72e5c1cd87b61f6d75a8637269b0d3f5106e7
omniaura <omniaura5@gmail.com>
2013-12-21 22:39:24 -0700

    content and location site models

--- content/models.py
+++ content/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    domain = models.OneToOneField('router.Domain', related_name='content')
+    body = models.TextField()
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='img')
