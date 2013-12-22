c61051ec1cce58b12425536bc78f5d3891d13921
omniaura <omniaura5@gmail.com>
2013-12-21 22:38:05 -0700

    started app 'router', added Domain model

---
+++ router/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Domain(models.Model):
+    url = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    LOCATION = 'lo'
+    CONTENT = 'ct'
+    KIND_CHOICES = ((LOCATION, 'location'), (CONTENT, 'content'))
+    kind = models.CharField(max_length=2, choices=KIND_CHOICES, 
+                            default=LOCATION) 
