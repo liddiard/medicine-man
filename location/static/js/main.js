d4c00feddff2a7a2e43e755c611164a3fb729bd1
omniaura <omniaura5@gmail.com>
2014-01-26 14:24:35 -0800

    rotator arrows, functionality

--- location/static/js/main.js
+++ location/static/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-    $('#rotator').flexslider({
-        animation: "slide",
+    $('.artwork, .plaque, .view-item').hide();
+    var piece = $('.artwork').first();
+    showPiece($('.artwork:visible'), piece);
+    $('.next').click(function(){
+        var current = $('.artwork:visible');
+        var next = current.next().length == 0 ? $('.artwork').first() : current.next();
+        showPiece(current, next);
+    });
+    $('.prev').click(function(){
+        var current = $('.artwork:visible');
+        var next = current.prev().length == 0 ? $('.artwork').last() : current.prev();
+        showPiece(current, next);
     });
 });
+
+function showPiece(prev, current) {
+    prev.fadeOut(1000);
+    prev.find('.plaque, .view-item').fadeOut(500);
+    current.fadeIn(1000);
+    setTimeout(function(){
+        current.find('.plaque').fadeIn(2000);
+    }, 2000);
+    setTimeout(function(){
+        current.find('.view-item').fadeIn(1000);
+    }, 4000);
+}
