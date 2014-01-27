9258b2509d960380a82cf6312b3775caaeb4e746
omniaura <omniaura5@gmail.com>
2014-01-26 18:52:59 -0800

    auto-advance rotator at interval

--- location/static/js/main.js
+++ location/static/js/main.js
@@ -2,28 +2,37 @@ $(document).ready(function(){
     $('.artwork, .plaque, .view-item').hide();
     var first = $('.artwork').first();
     var current = $('.artwork:visible') // we need to give showPiece an empty selector
-    showPiece(current, first, true);
+    rotator_intervalid = showPiece(current, first, true);
 });
 
 
 var bindArrows = function() {
     $('.next').click(function(){
-        var current = $('.artwork:visible');
-        var next = current.next().length == 0 ? $('.artwork').first() : current.next();
-        showPiece(current, next, true);
+        nextSlide();
     });
     $('.prev').click(function(){
-        var current = $('.artwork:visible');
-        var next = current.prev().length == 0 ? $('.artwork').last() : current.prev();
-        showPiece(current, next, false);
+        prevSlide();
     });
     $('.next, .prev').removeClass('disabled');
+}
+
+function nextSlide() {
+    var current = $('.artwork:visible');
+    var next = current.next().length == 0 ? $('.artwork').first() : current.next();
+    rotator_intervalid = showPiece(current, next, true);
+}
+
+function prevSlide() {
+    var current = $('.artwork:visible');
+    var next = current.prev().length == 0 ? $('.artwork').last() : current.prev();
+    rotator_intervalid = showPiece(current, next, false);
 }
 
 function showPiece(prev, current, forward) {
     // [jQuery obj] prev: artworks that should be hidden
     // [jQuery obj] current: artworks that should be shown
     // [boolean] forward: moving forward or backward? 
+    if (typeof rotator_intervalid != 'undefined') clearInterval(rotator_intervalid);
     $('.next, .prev').unbind('click').addClass('disabled'); // disable buttons while we're transitioning
     if (forward) {
         prev.animate({'background-position-x': '-=50', 'opacity': 0}, 1000).hide(1000);
@@ -40,4 +49,6 @@ function showPiece(prev, current, forward) {
         current.find('.view-item').fadeIn(1000);
         bindArrows(); // rebind next/prev buttons
     }, 4000);
+
+    return setInterval(function(){ nextSlide() }, 10000);
 }
