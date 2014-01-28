a16ecb41159f64cca76399dee43ad482f1372add
omniaura <omniaura5@gmail.com>
2014-01-27 20:00:04 -0800

    added help text to model fields

--- location/models.py
+++ location/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Site(models.Model):
     domain = models.OneToOneField('router.Domain', related_name='location')
     area = models.CharField(max_length=64)
+    area.help_text = '''
+        Should be in the form <strong>city, state</strong> (e.g. Santa Fe, NM) 
+        or <strong>city, country</strong> (e.g. Vienna, Austria)
+    '''
 
     def __unicode__(self):
         return self.domain.name
@@ -13,9 +17,12 @@ class Artwork(models.Model):
     image = models.ImageField(upload_to='location_artwork')
     title = models.CharField(max_length=64)
     date = models.CharField(max_length=32, blank=True)
+    date.help_text = "Time period created (e.g. 1920, September 1888, circa 1250)"
     artist = models.CharField(max_length=32, blank=True)
     location = models.CharField(max_length=32, blank=True)
+    location.help_text = "Current location of this piece (optional)"
     url = models.URLField(blank=True)
+    url.help_text = "Page to which to link a 'View Item' button (optional)"
 
     def __unicode__(self):
         return self.title
--- router/models.py
+++ router/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 class Domain(models.Model):
     url = models.CharField(max_length=64)
+    url.help_text = "Actual name of this domain (e.g. bostonartgallery.com)"
     name = models.CharField(max_length=64)
+    name.help_text = "Name to display on the site (e.g. Boston Art Gallery)"
     LOCATION = 'lo'
     CONTENT = 'ct'
     KIND_CHOICES = ((LOCATION, 'location'), (CONTENT, 'content'))
     kind = models.CharField(max_length=2, choices=KIND_CHOICES, 
                             default=LOCATION)
+    kind.help_text = '''
+        What type of site should this domain be? <br/>Be sure to create the 
+        corresponding <a href="../../../location/site/add/">location site</a> 
+        or <a href="../../../content/site/add/">content site</a>.
+    '''
     
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.url) 
