e5ab336d769f4f3abea1be6a026553f8f6b35651
omniaura <omniaura5@gmail.com>
2014-03-01 01:30:51 -0800

    PEP 8 compatability, utility function cleanup

--- location/views.py
+++ location/views.py
@@ -8,6 +8,8 @@ from router.models import Domain
 from .models import Site, Artwork
 
 
+# pages
+
 class FrontView(TemplateView):
 
     template_name = "location/front.html"
@@ -31,25 +33,25 @@ class FrontView(TemplateView):
         '''
         return HttpResponse(error_msg % domain_name, content_type='text/plain')
 
-def textToCoordinant(location):
-    latitude = ""
-    longitude = ""
-    locationSearch = location
-    locationUrl = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % locationSearch
-    #print locationUrl
-    response = urllib2.urlopen(locationUrl)
+
+# utility functions
+
+def text_to_coordinant(location):
+    location_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location
+    #print location_url
+    response = urllib2.urlopen(location_url)
     data = json.load(response)
     latitude = data["results"][0]["geometry"]["location"]["lat"]
     longitude = data["results"][0]["geometry"]["location"]["lng"]
     return latitude, longitude
 
-def coordinantToNearby(latitude, longitude):
-    location = "%s,%s" %(latitude, longitude)
+def coordinant_to_nearby(latitude, longitude):
+    location = "%s,%s" % (latitude, longitude)
     types = 'art_gallery'
-    nearbyUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=50000&types=%s&sensor=false&key=AIzaSyB6R31HHidq6Dm6qf6g1-c8iAKiadHq33o' %(location, types)
+    nearby_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=50000&types=%s&sensor=false&key=AIzaSyB6R31HHidq6Dm6qf6g1-c8iAKiadHq33o' % (location, types)
     response = urllib2.urlopen(nearbyUrl)
     data = json.load(response)
     print data
 
 
-#coordinantToNearby(textToCoordinant('Boston+MA')[0],textToCoordinant('Boston+MA')[1])
+#coordinant_to_nearby(text_to_coordinant('Boston+MA')[0],text_to_coordinant('Boston+MA')[1])
