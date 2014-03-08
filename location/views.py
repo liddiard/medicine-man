f7ca0c122f347147051c17fa528efca5b54dacba
omniaura <omniaura5@gmail.com>
2014-03-07 17:52:05 -0800

    urlencode querystring param, variable renaming, util func args improvement

--- location/views.py
+++ location/views.py
@@ -1,5 +1,6 @@
-import urllib2
 import json
+import urllib
+import urllib2
 
 from django.http import HttpResponse
 from django.views.generic import TemplateView
@@ -18,11 +19,15 @@ class FrontView(TemplateView):
         domain = self.kwargs['domain']
         context = self.get_context_data()
         try:
-            context['site'] = Site.objects.get(domain=domain)
+            site = Site.objects.get(domain=domain)
         except Site.DoesNotExist:
             domain_name = Domain.objects.get(id=domain)
             return self.site_dne_error(domain_name)
+        else:
+            context['site'] = site
         context['gallery'] = Artwork.objects.order_by('?')
+        coordinant = string_to_coordinant(site.area)
+        coordinant_to_nearby(coordinant)
         return self.render_to_response(context)
 
     def site_dne_error(self, domain_name):
@@ -36,22 +41,20 @@ class FrontView(TemplateView):
 
 # utility functions
 
-def text_to_coordinant(location):
-    location_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location
-    #print location_url
-    response = urllib2.urlopen(location_url)
+def string_to_coordinant(area):
+    area_clean = urllib.quote_plus(area)
+    q = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % area_clean
+    response = urllib2.urlopen(q)
     data = json.load(response)
-    latitude = data["results"][0]["geometry"]["location"]["lat"]
-    longitude = data["results"][0]["geometry"]["location"]["lng"]
+    latitude = data['results'][0]['geometry']['location']['lat']
+    longitude = data['results'][0]['geometry']['location']['lng']
     return latitude, longitude
 
-def coordinant_to_nearby(latitude, longitude):
-    location = "%s,%s" % (latitude, longitude)
-    types = 'art_gallery'
-    nearby_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=50000&types=%s&sensor=false&key=AIzaSyB6R31HHidq6Dm6qf6g1-c8iAKiadHq33o' % (location, types)
-    response = urllib2.urlopen(nearbyUrl)
+def coordinant_to_nearby(coordinant):
+    point = "%s,%s" % (coordinant[0], coordinant[1])
+    kind = "art_gallery"
+    q = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s&radius=50000&types=%s&sensor=false&key=AIzaSyB6R31HHidq6Dm6qf6g1-c8iAKiadHq33o" % (point, kind)
+    response = urllib2.urlopen(q)
     data = json.load(response)
     print data
 
-
-#coordinant_to_nearby(text_to_coordinant('Boston+MA')[0],text_to_coordinant('Boston+MA')[1])
