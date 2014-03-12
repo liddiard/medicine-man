125e96b82817dee55bcbc3807d33fdd1a1dedeff
omniaura <omniaura5@gmail.com>
2014-03-11 20:30:55 -0700

    restored show() method to previous position in chain

--- location/static/location/js/main.js
+++ location/static/location/js/main.js
@@ -56,10 +56,10 @@ function showPiece(prev, current, forward) {
         console.log(prev.css('background-image'));
         console.log(current.css('background-image'));
         prev.animate({'background-position-x': '-=50px', 'opacity': 0}, 1000).hide(1000);
-        current.show().css({'background-position-x': '50px', 'opacity': 0}).animate({'background-position-x': '-=50px', 'opacity': 1}, 1000);
+        current.css({'background-position-x': '50px', 'opacity': 0}).show().animate({'background-position-x': '-=50px', 'opacity': 1}, 1000);
     } else {
         prev.animate({'background-position-x': '+=50px', 'opacity': 0}, 1000).hide(1000);
-        current.show().css({'background-position-x': '-50px', 'opacity': 0}).animate({'background-position-x': '+=50px', 'opacity': 1}, 1000);
+        current.css({'background-position-x': '-50px', 'opacity': 0}).show().animate({'background-position-x': '+=50px', 'opacity': 1}, 1000);
     }
     prev.find('.plaque, .view-item').fadeOut(500);
     setTimeout(function(){
