694045ce5d17106bfb1bfc7d86a1bf4b2ceb13a2
omniaura <omniaura5@gmail.com>
2014-03-29 17:59:38 -0700

    fixed bug appending ADMINISTRATIVE_DOMAIN to ALLOWED_HOSTS

--- router/__init__.py
+++ router/__init__.py
@@ -8,4 +8,6 @@ settings.ALLOWED_HOSTS = [domain.url for domain in Domain.objects.all()]
 
 # make sure we could still access the admin domain even if the db were reset
 if settings.ADMINISTRATIVE_DOMAIN not in settings.ALLOWED_HOSTS:
-    settings.ALLOWED_HOSTS += settings.ADMINISTRATIVE_DOMAIN
+    settings.ALLOWED_HOSTS.append(settings.ADMINISTRATIVE_DOMAIN)
+
+print settings.ALLOWED_HOSTS
