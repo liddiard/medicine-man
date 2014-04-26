421472f5deced79c4a9eab5e5451cb6c3ab25144
omniaura <omniaura5@gmail.com>
2014-04-25 23:18:09 -0700

    management commands to import location and content domains (tested)

---
+++ router/management/commands/import_location_domains.py
@@ -0,0 +1,30 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from router.models import Domain
+from location.models import Site
+
+
+class Command(BaseCommand):
+    args = "<domains.csv>"
+    help = "Import location domains"
+
+    def handle(self, *args, **options):
+        try:
+            filename = args[0]
+        except IndexError:
+            self.stderr.write('Requires path to a .csv file as an argument')
+            return
+        num_domains_added = 0
+        with open(filename, 'rb') as file:
+            reader = csv.reader(file, delimiter=',')
+            for row in reader:
+                with transaction.atomic(): # make transaction succeed or 
+                                           # fail as a whole
+                    d = Domain(url=row[0], name=row[1], kind='lo')
+                    d.save()
+                    s = Site(domain=d, area=row[2])
+                    s.save()
+                num_domains_added += 1
+                self.stdout.write('added: %s' % d)
+        return "%d domains added" % num_domains_added
